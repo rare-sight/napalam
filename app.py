054397120d01be24a038c4cfe30996ef8fb38977
rare-sight <nikhilpanchal2724@gmail.com>
2024-10-04 20:39:50 +0530

    added new video and modified files

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import folium
 
@@ -10,34 +11,44 @@ def home():
 @app.route('/login', methods=['POST', 'GET'])
 def log():
     if request.method == 'POST':
-        latitude = float(request.form.get('latitude'))
-        longitude = float(request.form.get('longitude'))
-        y = float(request.form.get('y'))
+        radio = request.form.get('Fix')
+        if (radio == 'surface'):
+
+        
+            latitude = float(request.form.get('latitude'))
+            longitude = float(request.form.get('longitude'))
+            y = float(request.form.get('y'))
     
-        kappa = folium.Map(location=[latitude, longitude], zoom_start=13)   
-        folium.Circle(
+            kappa = folium.Map(location=[latitude, longitude], zoom_start=13)   
+            folium.Circle(
             location=[latitude, longitude],
             radius=120 * y ** (1/3),
             color='yellow',
             fill=True,
-        ).add_to(kappa)
+            ).add_to(kappa)
 
-        folium.Circle(
+            folium.Circle(
             location=[latitude, longitude],
             radius=239*y**(0.427),
             color='green',
-            fill=True,
-        ).add_to(kappa)
+                fill=True,
+            ).add_to(kappa)
 
-        folium.Circle(
+            folium.Circle(
             location=[latitude, longitude],
             radius=218*y**0.334,
             color='red',
             fill=True,
-        ).add_to(kappa)
-        
-        kappa.save('templates/map.html')
-        return render_template('map.html')
+            ).add_to(kappa)
+
+            map_path = os.path.join(app.root_path, 'templates', 'map.html')
+            kappa.save(map_path)
+
+            return render_template('map.html')
+        else :
+            return "hello there"
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
